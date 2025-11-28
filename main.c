5b709b0e195eb04804b7ed546ce2bb8941500a14
Link2004tw <fa.csf2023@gmail.con>
2025-11-28 14:13:41 +0200

    added libraries

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdint.h>
-#include "consts.h"
+//#include "consts.h"
+#include <stdint.h>
+#include <stdbool.h>        // four macros for boolean
+#include "tm4c123gh6pm.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"   // for clock gating API
+#include "driverlib/gpio.h"     // for gpio APIs & GPIO_PINx definition
+#include "inc/hw_memmap.h"      // GPIO_PORTx_BASE definition
+
 int main()
 {
   return 0;
