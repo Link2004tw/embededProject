428bb09e5cdda458a339f5cd5a44490f1ca3eb62
Farida Tawfik <ferytawfik90@gmail.com>
2025-12-04 16:13:50 +0200

    frontend send message

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include <stdint.h>
 //#include "consts.h"
-#include <stdbool.h>        // four macros for boolean
+#include <stdbool.h> // four macros for boolean
 #include "tm4c123gh6pm.h"
 #include "inc/hw_types.h"
-#include "driverlib/sysctl.h"   // for clock gating API
-#include "driverlib/gpio.h"     // for gpio APIs & GPIO_PINx definition
-#include "inc/hw_memmap.h"      // GPIO_PORTx_BASE definition
-#include "driverlib/i2c.h"
+#include "driverlib/sysctl.h" // for clock gating API
+#include "driverlib/gpio.h" // for gpio APIs & GPIO_PINx definition
+#include "inc/hw_memmap.h"
+#include "driverlib/uart.h"
 #include "driverlib/pin_map.h"
-#include "eepromDriver.h"
+#include "HMI_Comm.h"
 
 int main()
-{
-  return 0;
+{ 
+  UART0_Init();
+  // LCD_Init();
+  // Keypad_Init();
+  //uint8_t testPass[5] = {'1','2','3','4','5'};
+  
+   while(1) {
+     //GetUserPassword(PasswordBuffer); // keypad module fills the array
+     HMI_SendPassword(PasswordBuffer, 5);
+   }
 }
